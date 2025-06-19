47e883962b0c4a953730f4dcec12681c1e69aca0
GabrielMacedo07 <macedo425@gmail.com>
2025-06-19 16:39:36 -0300

    número limite 50

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -26,7 +26,7 @@ function exibirTextoNaTela(tag, texto) {
 //boa prática para não ficar na repetição de códigos...
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do número secreto' );
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
